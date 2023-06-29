90f3faf9b65cf4473670c5840eec2068f918c9ed
neeti-nk47 <neeti.nk47@gmail.com>
2023-06-29 17:53:35 +0530

    completed delete button working

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import Link from "next/link";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -8,6 +8,12 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Todo App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossorigin="anonymous"
+        />
       </Head>
 
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -40,40 +40,72 @@ export default function Home() {
     }
   };
 
+  const deleteTask = async (taskId) => {
+    try {
+      await axios.delete(`/api/tasks/${taskId}`);
+      fetchTasks();
+    } catch (error) {
+      console.error("Failed to delete task:", error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Todo App</h1>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-        placeholder="Enter a task"
-      />
-      <button onClick={addTask}>Add Task</button>
+    <div className="container w-50 p-3 ">
+      <div className="card bg-light">
+        <h1 className="text-center">Todo App</h1>
+        <div className="card-body text-center">
+          <input
+            className="form-control "
+            type="text"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="Enter a task"
+          />
+          <button
+            className=" form-control-sm btn btn-primary m-3"
+            onClick={addTask}
+          >
+            Add Task
+          </button>
 
-      <div>
-        <h2>Active Tasks:</h2>
-        {tasks.map(
-          (task) =>
-            !task.completed && (
-              <div key={task._id}>
-                <p>{task.name}</p>
-                <button onClick={() => completeTask(task._id)}>Complete</button>
-              </div>
-            )
-        )}
-      </div>
+          <div className="card p-3 mb-5 bg-tertiary">
+            <h2>Active Tasks:</h2>
+            {tasks.map(
+              (task) =>
+                !task.completed && (
+                  <ul className="list-group" key={task._id}>
+                    <li className="list-group-item">
+                      {task.name}{" "}
+                      <button
+                        className="btn btn-sm btn-success"
+                        onClick={() => completeTask(task._id)}
+                      >
+                        Completed
+                      </button>
+                      <button
+                        className="btn btn-sm btn-danger"
+                        onClick={() => deleteTask(task._id)}
+                      >
+                        Delete
+                      </button>
+                    </li>
+                  </ul>
+                )
+            )}
+          </div>
 
-      <div>
-        <h2>Completed Tasks:</h2>
-        {tasks.map(
-          (task) =>
-            task.completed && (
-              <div key={task._id}>
-                <p>{task.name}</p>
-              </div>
-            )
-        )}
+          <div className="card p-3 mb-5 ">
+            <h2>Completed Tasks:</h2>
+            {tasks.map(
+              (task) =>
+                task.completed && (
+                  <ul className="list-group" key={task._id}>
+                    <li className="list-group-item">{task.name}</li>
+                  </ul>
+                )
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
